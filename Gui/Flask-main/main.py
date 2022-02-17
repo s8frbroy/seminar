2235c5eb5df8b9ebd0a94997fd8bb85ce57ada24
s8frbroy <broyfrederik01@gmail.com>
2022-02-17 17:53:09 +0100

    dropdown menu frontpage

--- Gui/Flask-main/main.py
+++ Gui/Flask-main/main.py
@@ -5,23 +5,31 @@ from werkzeug.utils import secure_filename
 import os
 from wtforms.validators import InputRequired
 
+# create Flask app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
+
 
 class UploadFileForm(FlaskForm):
     file = FileField("File", validators=[InputRequired()])
     submit = SubmitField("Upload File")
 
-@app.route('/', methods=['GET',"POST"])
-@app.route('/home', methods=['GET',"POST"])
+
+@app.route('/', methods=['GET', "POST"])
+@app.route('/home', methods=['GET', "POST"])
 def home():
     form = UploadFileForm()
+    datatype = ['Multi', 'HouseA', 'HouseB']
+    model = ['LSTM', 'CNN', 'SPEED']
     if form.validate_on_submit():
-        file = form.file.data # First grab the file
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
+        file = form.file.data  # First grab the file
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)), app.config['UPLOAD_FOLDER'],
+                               secure_filename(file.filename)))  # Then save the file
         return "File has been uploaded."
-    return render_template('index.html', form=form)
+    return render_template('index.html', form=form, datatype=datatype, model = model)
+
 
 if __name__ == '__main__':
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug=True)
